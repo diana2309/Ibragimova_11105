4a2085af14478f73e609c50cf3db182e279e313e
diana2309 <91032213+diana2309@users.noreply.github.com>
2022-04-27 14:13:28 +0300

    Update task014.py

--- task014.py
+++ task014.py
@@ -2,8 +2,8 @@ import re
 
 def task014():
     with open('smth.txt', encoding='utf8') as file:
-    pattern = r'\w*a+[^b]\b'
-    string = file.read()
-    return re.findall(pattern, string)
+        pattern = r'\w*a+[^b]\b'
+        string = file.read()
+        return re.findall(pattern, string)
 
 print(d)
