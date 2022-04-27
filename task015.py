459dc6d869d6b5da57759a2637f3c5f59a54233c
diana2309 <91032213+diana2309@users.noreply.github.com>
2022-04-27 14:15:35 +0300

    Update task015.py

--- task015.py
+++ task015.py
@@ -1,5 +1,8 @@
 import re
-pattern = r'(a+[a[^13579]*[b[2468]]+\s)|(a+[a{2}]*c{3}[b{2}]+\s)|(a+[a{2}]*c{5}[b{2}]+\s)'
-string = 'b a ab aaabbb aaacccbbbb aaaccccbbbb aaacccccbb aaaabb aaaaabb'
-d = re.findall(pattern, string)
+
+def task016():
+    with open('tsk14-18.txt', encoding='utf8') as file:
+        pattern = r'(a+[a[^13579]*[b[2468]]+\s)|(a+[a{2}]*c{3}[b{2}]+\s)|(a+[a{2}]*c{5}[b{2}]+\s)'
+        string = file.read()
+        return re.findall(pattern, string)
 print(d)
