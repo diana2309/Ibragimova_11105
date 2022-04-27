fc71e554d5f2fa871439ae6307852a1392bc6853
diana2309 <91032213+diana2309@users.noreply.github.com>
2022-04-27 14:10:30 +0300

    Update task014.py

--- task014.py
+++ task014.py
@@ -1,6 +1,9 @@
 import re
-pattern = r'\w*a+[^b]\b'
-string = 'aaab aaaac aaad ae ad b aed'
-d = re.findall(pattern, string)
+
+def task014():
+    with open('smth.txt', encoding='utf8') as file:
+    pattern = r'\w*a+[^b]\b'
+    string = file.read()
+    return re.findall(pattern, string)
 
 print(d)
