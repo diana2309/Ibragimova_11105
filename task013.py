d0265d733980d57dda9b44d6cc30697542494eae
diana2309 <91032213+diana2309@users.noreply.github.com>
2022-05-29 18:16:03 +0300

    Update task013.py

--- task013.py
+++ task013.py
@@ -1,13 +1,17 @@
 from unittest import TestCase, main
 from task011 import exponentiation
 from task012 import calculate
+from task012 import cur1_calculate
 
 class Tests(TestCase):
-    def test_type(self):
-        self.assertEqual(exponentiation('3.45', 45, 67), int)
+    def test_equal(self):
+        self.assertEqual(exponentiation(1, 2, 1), 3336.2175999999995)
 
     def test_calculation(self):
         self.assertEqual(calculate(1, '+', 1, '+', 1, '+', 1, '+', 1), 5)
+
+    def test_cur1(self):
+        self.assertEqual('hgfjdks', int)
 
 
 if __name__ == '__main__':
