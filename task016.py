963b1c801624a68d06fcf077cf89f00ade74eed0
diana2309 <91032213+diana2309@users.noreply.github.com>
2022-05-29 14:47:24 +0300

    Update task016.py

--- task016.py
+++ task016.py
@@ -1,6 +1,6 @@
 import re
 
-def task015():
+def task016():
     with open('smth.txt', encoding='utf8') as file:
         pattern = r'(?:[01][0-9]:[0-5][0-9])|(?:2[0-3]:[0-5][0-9])\s'
         string = file.read()
