094b73f471b08c2263678886b2c1fb734fdd4ecf
diana2309 <91032213+diana2309@users.noreply.github.com>
2022-05-29 14:46:55 +0300

    Update task015.py

--- task015.py
+++ task015.py
@@ -5,4 +5,3 @@ def task015():
         pattern = r'(a+[a[^13579]*[b[2468]]+\s)|(a+[a{2}]*c{3}[b{2}]+\s)|(a+[a{2}]*c{5}[b{2}]+\s)'
         string = file.read()
         return re.findall(pattern, string)
-print(d)
