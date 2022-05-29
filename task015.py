dc1290b5be197abd8a43d12c3824a198e646b446
diana2309 <91032213+diana2309@users.noreply.github.com>
2022-05-29 14:45:35 +0300

    Update task015.py

--- task015.py
+++ task015.py
@@ -1,7 +1,7 @@
 import re
 
-def task016():
-    with open('tsk14-18.txt', encoding='utf8') as file:
+def task015():
+    with open('smth.txt', encoding='utf8') as file:
         pattern = r'(a+[a[^13579]*[b[2468]]+\s)|(a+[a{2}]*c{3}[b{2}]+\s)|(a+[a{2}]*c{5}[b{2}]+\s)'
         string = file.read()
         return re.findall(pattern, string)
