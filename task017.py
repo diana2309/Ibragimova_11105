9b9eedfe531068a8c7fe0fb7bc1bff0c4cd56fd0
diana2309 <91032213+diana2309@users.noreply.github.com>
2022-05-29 14:48:12 +0300

    Update task017.py

--- task017.py
+++ task017.py
@@ -1,7 +1,7 @@
 import re
 
 def task017():
-    with open('tsk14-18.txt', encoding='utf8') as file:
+    with open('smth.txt', encoding='utf8') as file:
         pattern = r'[А-Я]+[^а-я]+'
         string = file.read()
         return re.findall(pattern, string)
